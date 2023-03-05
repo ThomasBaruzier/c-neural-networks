8d7dd1184f7ee828e16624fa3a4487a39a9975c2
ThomasBaruzier <thomas.baruzier@gmail.com>
2023-03-05 16:17:26 +0100

    dynamic allocations + neurons initialization

--- nn.c
+++ nn.c
@@ -15,6 +15,7 @@ struct HyperParams {
   size_t numOutputs;
   size_t numSets;
   size_t numEpochs;
+  size_t numParams;
   size_t numLayers;
   size_t *layerSizes;
   float learningRate;
@@ -23,9 +24,42 @@ struct HyperParams {
 struct Neurons {
   float (*dataset)[1][1];
   float (*weights)[1][1];
-  float **biases;
-  float **values;
+  float (*biases)[1];
+  float (*values)[1];
 };
+
+void train(FILE *fp);
+void askParams(struct HyperParams *hp);
+void freeAll(struct Neurons *n, struct HyperParams *hp);
+int detectMode(char *path);
+int parseFile(FILE* fp, char **data, size_t *size, struct HyperParams *hp);
+size_t loadData(char *data, size_t size, struct Neurons *n, struct HyperParams *hp);
+size_t initNeurons(size_t *size, struct Neurons *n, struct HyperParams *hp);
+
+int main(int argc, char **argv)
+{
+  if (argc != 2) {
+    printf("Usage : %s <dataset.ds>|<model.ml>\n", argv[0]);
+    return 1;
+  }
+
+  char *path = argv[1];
+  FILE *fp = fopen(path, "r");
+  if (fp == NULL) {
+    perror("ERROR : File doesn't exist\n");
+    exit(1);
+  }
+
+  if (detectMode(path)) {
+    printf("\nTraining using %s\n\n", path);
+    train(fp);
+  } else {
+    printf("\nRunning %s\n\n", path);
+    //run(path);
+  }
+
+  return 0;
+}
 
 int detectMode(char *path) {
   int i=0;
@@ -33,15 +67,57 @@ int detectMode(char *path) {
   if (path[i] == 'd' && path[i+1] == 's') return 1;
   if (path[i] == 'm' && path[i+1] == 'l') return 0;
   else {
-    printf("ERROR : Unrecognized file extension\n");
+    perror("ERROR : Unrecognized file extension\n");
     exit(1);
+  }
+}
+
+void train(FILE *fp) {
+  char *data;
+  size_t size;
+  struct Neurons n;
+  struct HyperParams hp;
+
+  askParams(&hp);
+  parseFile(fp, &data, &size, &hp);
+  printf("\nDetected %zu sets, %zu inputs, %zu outputs\n",
+    hp.numSets, hp.numInputs, hp.numOutputs);
+  printf("Successfully loaded the dataset (%.2f MB)\n",
+    loadData(data, size, &n, &hp) / (1.0*MB));
+  initNeurons(&size, &n, &hp);
+  printf("Successfully initialized %zu parameters (%.2f MB)\n",
+    hp.numParams, size / (1.0*MB));
+  printf("\n");
+
+  freeAll(&n, &hp);
+}
+
+void askParams(struct HyperParams *hp) {
+  int layerSize;
+
+  do {
+    printf("Number of epochs : ");
+    scanf(" %d", &hp->numEpochs);
+  } while (hp->numEpochs < 1);
+
+  do {
+    printf("Number of layers : ");
+    scanf(" %d", &hp->numLayers);
+  } while (hp->numLayers < 1);
+  hp->numLayers += 2;
+  hp->layerSizes = malloc(sizeof(float *) * hp->numLayers);
+
+  for (int i=1; i < hp->numLayers-1; i++) {
+    printf("Size of layer %d : ", i);
+    scanf(" %d", &layerSize);
+    hp->layerSizes[i] = layerSize;
   }
 }
 
 int parseFile(FILE* fp, char **data, size_t *size, struct HyperParams *hp) {
   fseek(fp, 0L, SEEK_END); *size = ftell(fp); rewind(fp);
   *data = mmap(NULL, *size, PROT_READ, MAP_PRIVATE, fileno(fp), 0);
-  if (*data == MAP_FAILED) { perror("Not enough memory"); exit(1); }
+  if (*data == MAP_FAILED) { perror("ERROR : Not enough memory"); exit(1); }
 
   size_t i=0;
   hp->numInputs = 1; hp->numOutputs = 1; hp->numSets = 1;
@@ -52,6 +128,8 @@ int parseFile(FILE* fp, char **data, size_t *size, struct HyperParams *hp) {
   for (; (*data)[i] != ' ' && i < *size; i++)
     if ((*data)[i] == ',') hp->numOutputs++;
 
+  hp->layerSizes[0] = hp->numInputs;
+  hp->layerSizes[hp->numLayers-1] = hp->numOutputs;
   return 0;
 }
 
@@ -81,39 +159,49 @@ size_t loadData(char *data, size_t size, struct Neurons *n, struct HyperParams *
   return toAlloc;
 }
 
-void train(FILE *fp) {
-  char *data;
-  size_t size;
-  struct Neurons n;
-  struct HyperParams hp;
+size_t initNeurons(size_t *size, struct Neurons *n, struct HyperParams *hp) {
+  size_t i, j, k, neuronCount = 0, weightCount = 0;
 
-  parseFile(fp, &data, &size, &hp);
-  printf("Detected %zu sets, %zu inputs, %zu outputs\n", hp.numSets, hp.numInputs, hp.numOutputs);
-  printf("Successfully loaded the dataset (%.2f MB)\n", loadData(data, size, &n, &hp) / (1.0*MB));
-  printf("\n");
-}
-
-int main(int argc, char **argv)
-{
-  if (argc != 2) {
-    printf("Usage : %s <dataset.ds>|<model.ml>\n", argv[0]);
-    return 1;
+  for (i=0; i < hp->numLayers; i++) {
+    neuronCount += hp->layerSizes[i];
+    if (i+1 < hp->numLayers)
+      weightCount += hp->layerSizes[i] * hp->layerSizes[i+1];
   }
 
-  char *path = argv[1];
-  FILE *fp = fopen(path, "r");
-  if (fp == NULL) {
-    printf("ERROR : File doesn't exist\n");
+  *size = sizeof(float) * ((neuronCount * 2) + weightCount);
+  hp->numParams = neuronCount + weightCount - hp->layerSizes[0];
+  n->biases = malloc(sizeof(float) * neuronCount);
+  n->values = malloc(sizeof(float) * neuronCount);
+  n->weights = malloc(sizeof(float) * weightCount);
+
+  if (n->biases == NULL || n->values == NULL || n->weights == NULL) {
+    perror("ERROR : Not enough memory");
     exit(1);
   }
 
-  if (detectMode(path)) {
-    printf("\nTraining using %s\n", path);
-    train(fp);
-  } else {
-    printf("\nRunning %s\n", path);
-    //run(path);
-  }
+  for (i=0; i+1 < hp->numLayers; i++)
+    for (j=0; j < hp->layerSizes[i]; j++)
+      for (k=0; k < hp->layerSizes[i+1]; k++)
+        n->weights[i][j][k] = randomValue();
+        // printf("n->weights[%zu][%zu][%zu] = %f\n", i, j, k, n->weights[i][j][k]);
+
+  for (i=0; i < hp->numLayers; i++)
+    for (j=0; j < hp->layerSizes[i]; j++)
+      n->biases[i][j] = randomValue();
+      // printf("n->biases[%zu][%zu] = %f\n", i, j, n->biases[i][j]);
+
+  for (i=0; i < hp->numLayers; i++)
+    for (j=0; j < hp->layerSizes[i]; j++)
+      n->values[i][j] = randomValue();
+      // printf("n->values[%zu][%zu] = %f\n", i, j, n->values[i][j]);
 
   return 0;
+}
+
+void freeAll(struct Neurons *n, struct HyperParams *hp) {
+  free(hp->layerSizes);
+  free(n->dataset);
+  free(n->weights);
+  free(n->biases);
+  free(n->values);
 }
